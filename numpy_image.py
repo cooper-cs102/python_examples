bc6bdd9a010bda8a487a55ae7b9b2e088fd79d19
cooper-cs102 <cs102@astrocox.com>
2019-12-11 18:43:14 -0500

    Numpy image example with mask

--- numpy_image.py
+++ numpy_image.py
@@ -13,12 +13,22 @@ print(f"Original image is {width} x {height} pixels")
 
 # Convert image to a numpy array
 pixel_array = np.array(img)
-print(f"numpy array dimensions before transpose: {pixel_array.shape}")
+max_value = np.max(pixel_array)
+print(f"Max value: {max_value}")
+x_center = pixel_array.shape[0] / 2
+y_center = pixel_array.shape[1] / 2
 
-# Transform the array
-pixel_array = np.transpose(pixel_array, axes=[1, 0, 2])
-print(f"numpy array dimensions after transpose: {pixel_array.shape}")
+
+def mask(x, y, z):
+    distance_from_center = np.sqrt(np.square(x - x_center) + np.square(y - y_center))
+    max_val = np.max(distance_from_center)
+    return distance_from_center / max_val
+
+
+mask_array: np.ndarray = np.fromfunction(mask, pixel_array.shape)
+output_array = np.multiply(mask_array, pixel_array)
+output_array = output_array.astype(np.uint8)
 
 # Pillow has built-in support for creating an Image from a numpy array
-new_img = Image.fromarray(pixel_array)
+new_img = Image.fromarray(output_array)
 new_img.save(output_path)
